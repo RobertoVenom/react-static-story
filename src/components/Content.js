fa679739c3d31e63330248e8bd3856061f3d3fe9
RobertoVenom <156466142+RobertoVenom@users.noreply.github.com>
2026-01-26 16:18:22 -0500

    Add files via upload

---
+++ src/components/Content.js
@@ -0,0 +1,22 @@
+function Content() {
+  return (
+    <main>
+      <p>
+        The Hare was very proud of how fast he could run and often mocked the slow-moving Tortoise.
+        Tired of the teasing, the Tortoise challenged the Hare to a race.
+      </p>
+
+      <p>
+        When the race began, the Hare quickly ran far ahead and decided to take a nap, confident
+        that he would win easily.
+      </p>
+
+      <p>
+        Meanwhile, the Tortoise kept moving slowly but steadily without stopping.
+        While the Hare slept, the Tortoise crossed the finish line and won the race.
+      </p>
+    </main>
+  );
+}
+
+export default Content;
